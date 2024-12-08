4f9bcc5731103e9d23f55d2f207aabb3b1c112cb
David Diomede <david@d2creativedesign.com>
2024-12-08 18:03:11 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
-import Image from "next/image";
+'use client';
+
+import { useForm, SubmitHandler } from "react-hook-form"
+import { useState } from "react"
+
+interface FormValues {
+    baseSize: string;
+    exampleRequired: string;
+    typeScale: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [formData, setFormData] = useState<FormValues | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm<FormValues>({
+        defaultValues: {
+            baseSize: "16",
+            typeScale: "1.125"
+        }
+    });
+
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        setFormData(data);
+    };
+
+    console.log(watch("baseSize")); // you can watch individual input by pass the name of the input
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset>
+                    <legend>Common values</legend>
+                    <label>Base size (px)</label>
+                    <input {...register("baseSize")} defaultValue="16"/>
+
+
+                    <label>Scale</label>
+                    <div className="custom-select">
+                        <select {...register("typeScale")}>
+                            <option value="1.125">Major Second</option>
+                            <option value="1.25">Major Third</option>
+                            <option value="1.133">Perfect Fourth</option>
+                            <option value="1.618">Golden Ratio</option>
+                        </select>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Typography</legend>
+                    <input type="submit"/>
+                </fieldset>
+            </form>
+            {formData && (
+                <div className="text-white">
+                    <h2>Form Data</h2>
+                    <p>Example: {Number(formData.baseSize) * Number(formData.typeScale)}</p>
+                    <p>Type Scale: {formData.typeScale}</p>
+                </div>
+            )}
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
